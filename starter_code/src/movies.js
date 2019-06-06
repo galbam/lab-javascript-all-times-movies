265abd90b208129e99f4497864361cba0076fee1
mjarraya <mjarraya@student.42.fr>
2019-06-06 22:59:16 +0200

    fixed tests

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,13 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
-
-
-// Get the average of all rates with 2 decimals 
-
+// Get the average of all rates with 2 decimals
 
 // Get the average of Drama Movies
 
+// Order by time duration, ascending
 
-// Order by time duration, in growing order
-
-
-// How many movies did STEVEN SPIELBERG
-
+// How many movies did Steven Spielberg direct
 
 // Order by title and print the first 20 titles
 
-
 // Best yearly rate average
+
+// Turn duration of the movies from hours to minutes
